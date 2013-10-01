27588d899d655b42fd159abe85dea54af838ba66
Darren Boss <darren.boss@gmail.com>
2013-09-30 20:05:37 -0400

    Cache the csv file and use the local copy if exists

--- src/wepay_transactions.py
+++ src/wepay_transactions.py
@@ -20,9 +20,30 @@ LOGIN = '/login'
 CSV_URL = WEPAY_URL + CSV
 LOGIN_URL = WEPAY_URL + LOGIN
 
+
+def download_csv(username):
+    password = getpass.getpass("Password:")
+    s = requests.Session()
+    html_doc = s.get(LOGIN_URL)
+    soup = BeautifulSoup(html_doc.text)
+    csrf = soup.find('input', attrs={'name': 'csrf'})
+    nonce = soup.find('input', attrs={'name': 'nonce'})
+    print csrf['value']
+    print nonce['value']
+    payload = {'email': username, 'password': password,
+               'csrf': csrf['value'], 'nonce': nonce['value']}
+    r = s.post(LOGIN_URL, payload)
+    #print r.text
+    csv_file = s.get(url=CSV_URL)
+    f = open('transactions.csv', 'w')
+    f.write(csv_file.text)
+    f.close()
+
 parser = argparse.ArgumentParser(description='Download WePay transactions.')
+parser.add_argument('-d', '--download', action='store_true',
+                    help='download transactions')
 parser.add_argument('-u', '--username', dest='username',
-                    required=True, help='WePay username')
+                    help='WePay username')
 parser.add_argument('-m', dest='month', type=str,
                     help='only display totals for the month')
 parser.add_argument('-y', dest='year', type=str,
@@ -32,26 +53,15 @@ parser.add_argument('-e', dest='email', type=str,
 parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
                     default=sys.stdout)
 args = parser.parse_args()
-password = getpass.getpass("Password:")
-s = requests.Session()
-html_doc = s.get(LOGIN_URL)
-soup = BeautifulSoup(html_doc.text)
-csrf = soup.find('input', attrs={'name': 'csrf'})
-nonce = soup.find('input', attrs={'name': 'nonce'})
-print csrf['value']
-print nonce['value']
-payload = {'email': args.username, 'password': password,
-           'csrf': csrf['value'], 'nonce': nonce['value']}
-r = s.post(LOGIN_URL, payload)
-#print r.text
-csv_file = s.get(url=CSV_URL)
-#print csv_file.text
-data = csv_file.text
+if args.download:
+    download_csv(args.username)
+with open('transactions.csv', 'r') as content_file:
+    data = content_file.read()
 now = datetime.datetime.now()
 year = now.year
 total = 0
 content = csv.DictReader(data.splitlines())
-#print data 
+#print data
 if args.email:
     for row in content:
         if row['Email'] == args.email and row['Status'] == 'Complete':
